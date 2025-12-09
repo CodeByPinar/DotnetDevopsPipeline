37b4bb10706e8f109d10ce9a83d0c5bec4e9ceaf
ThecoderPinar <thecoderpinar@gmail.com>
2025-12-09 15:38:11 +0300

    Initial DevOps pipeline setup

---
+++ DotnetDevopsPipeline.Api/Program.cs
@@ -0,0 +1,31 @@
+﻿using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging(); // ✅ HTTP request logları
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHealthChecks("/health");
+
+app.Run();
